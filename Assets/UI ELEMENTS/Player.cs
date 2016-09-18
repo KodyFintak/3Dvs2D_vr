e330adafc0776d2685fdda68da72e3b61ba774eb
Allen Ng <allenng@buffalo.edu>
2016-09-18 19:11:59 -0400

    Pushing V0.1.0 - Added house

---
+++ Assets/UI ELEMENTS/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private Stat health;
+    [SerializeField]
+    private Stat mana;
+    [SerializeField]
+    private Stat xp;
+
+    private void Awake()
+    {
+        health.Initialize();
+        mana.Initialize();
+        xp.Initialize();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Y))
+        {
+            health.CurrentVal -= 1;
+        }
+        if (Input.GetKeyUp(KeyCode.U))
+        {
+            health.CurrentVal += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            mana.CurrentVal -= 1;
+        }
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            mana.CurrentVal += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            xp.CurrentVal -= 1;
+        }
+        if (Input.GetKeyUp(KeyCode.M))
+        {
+            xp.CurrentVal += 1;
+        }
+    }
+}
---
+++ Assets/myAssets/Benny Castle Gate Opener.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Benny_Castle_Gate_Opener : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("f")){
+			gameObject.SetActive(false);
+		}
+	}
+}
