00220d7945cdab012235dbdc974adad141a89047
Matthew J Luck <mjluck@buffalo.edu>
2016-10-27 22:34:21 -0400

    Options Menu created, Set name in options menu available, New Scripts: GameManager, PlayerManager, CameraWork, PlayerNameInputField - edited Scripts: PhotonNetworkManager1 -- backup before continuing on.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using System;
+using System.Collections;
+
+public class GameManager : Photon.PunBehaviour {
+    [SerializeField]
+    SpawnSpot[] spawnSpots;
+
+
+    public void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+	
+	// Update is called once per frame
+	public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    void Start()
+    {
+        spawnSpots = GameObject.FindObjectsOfType<SpawnSpot>();
+
+    }
+
+    void LoadArena()
+    {
+        if ( ! PhotonNetwork.isMasterClient )
+        {
+            Debug.LogError("PhotonNetwork: Trying to Load a level but we are not the master Client.");
+        }
+        Debug.Log("PhotonNetwork : Loading Level : 1");
+        PhotonNetwork.LoadLevel("Level1");
+    }
+
+    #region Photon Messages
+
+    public override void OnPhotonPlayerConnected(PhotonPlayer other)
+    {
+        Debug.Log("OnPhotonPlayerConnected() " + other.name); // not seen if you're the player connecting
+
+        if (PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+            LoadArena();
+        }
+    }
+
+    public override void OnPhotonPlayerDisconnected(PhotonPlayer other)
+    {
+        Debug.Log("OnPhotonPlayerDisconnected() " + other.name); // seen when other disconnects
+
+        if (PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+            LoadArena();
+        }
+    }
+
+    #endregion
+
+}
--- Assets/Scripts/PhotonNetworkManager1.cs
+++ Assets/Scripts/PhotonNetworkManager1.cs
@@ -5,157 +5,168 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public class PhotonNetworkManager1 : Photon.PunBehaviour {
-    
-	public static PhotonNetworkManager1 instance = null;
+public class PhotonNetworkManager1 : Photon.PunBehaviour
+{
 
+    #region Global Variables
+
+    public static PhotonNetworkManager1 instance = null;
+
+    #endregion
+
+    #region Public Variables
+
+    public InputField room_name;
+    public GameObject roomPrefab;
+    public PhotonLogLevel LogLevel = PhotonLogLevel.Informational;
     [SerializeField]
     Text connectionText;
     [SerializeField]
     Transform[] spawnPoints;
-    [SerializeField]
-    Camera sceneCamera;
 
-    public InputField room_name;
-	public GameObject roomPrefab;
-	private List<GameObject> roomPrefabs = new List<GameObject>();
+    #endregion
+
+    #region Private Variables
+
+    private List<GameObject> roomPrefabs = new List<GameObject>();
     GameObject player;
+    string _gameVersion = "0.2.0";
 
-	void Awake()
-	{
-		if (instance == null) {
-			instance = this;
-			DontDestroyOnLoad (gameObject.transform);
-		} else if (instance != this) {
-			Destroy (gameObject);
-		}
-	}
+    #endregion
 
-	void Start()
-	{
-		PhotonNetwork.ConnectUsingSettings ("0.2.0");
-        //PhotonNetwork.automaticallySyncScene = true;
-	}
+    #region Photon.PunBehaviour Callbacks
 
-    void Update()
+    void Awake()
     {
-        connectionText.text = PhotonNetwork.connectionStateDetailed.ToString();
+        //if (instance == null)
+        //{
+        //    instance = this;
+        //    DontDestroyOnLoad(gameObject.transform);
+        //}
+        //else if (instance != this)
+        //{
+        //    Destroy(gameObject);
+        //}
+        PhotonNetwork.logLevel = LogLevel;
+
+        PhotonNetwork.autoJoinLobby = false;
+        PhotonNetwork.automaticallySyncScene = true;
+    }
+
+    void Start()
+    {
+    }
+
+    void RefreshRoomList()
+    {
+        if (roomPrefabs.Count > 0)
+        {
+            for (int i = 0; i < roomPrefabs.Count; i++)
+            {
+                Destroy(roomPrefabs[i]);
+            }
+
+            roomPrefabs.Clear();
+        }
+
+        for (int i = 0; i < PhotonNetwork.GetRoomList().Length; i++)
+        {
+            GameObject g = Instantiate(roomPrefab);
+            g.transform.SetParent(roomPrefab.transform.parent);
+
+            g.GetComponent<RectTransform>().localScale = roomPrefab.GetComponent<RectTransform>().localScale;
+            g.GetComponent<RectTransform>().localPosition = new Vector3(roomPrefab.GetComponent<RectTransform>().localPosition.x, roomPrefab.GetComponent<RectTransform>().localPosition.y - (i * 50), roomPrefab.GetComponent<RectTransform>().localPosition.z);
+            g.transform.FindChild("Room_Name_Text").GetComponent<Text>().text = PhotonNetwork.GetRoomList()[i].name;
+            g.transform.FindChild("Room_Info").GetComponent<Text>().text = PhotonNetwork.GetRoomList()[i].playerCount + "/" + PhotonNetwork.GetRoomList()[i].maxPlayers;
+
+            g.transform.FindChild("Join").GetComponent<Button>().onClick.AddListener(() => { JoinRoom(g.transform.FindChild("Room_Name_Text").GetComponent<Text>().text); });
+
+            g.SetActive(true);
+            roomPrefabs.Add(g);
+        }
+    }
+
+    void JoinRandomRoomButton()
+    {
+        if (PhotonNetwork.GetRoomList().Length > 0)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            Debug.LogError("No Rooms");
+        }
     }
 
 
-	public void ButtonEvents(string EVENT)
-	{
-		switch (EVENT) {
-		case "CreateRoom":
-			if (PhotonNetwork.JoinLobby ()) {
-				
-				RoomOptions RO = new RoomOptions ();
-				RO.MaxPlayers = 4;
-				PhotonNetwork.CreateRoom (room_name.text,RO,TypedLobby.Default);
+    void JoinRoom(string roomName)
+    {
 
-			}
-			break;
-		case "RefreshButton":
-			if(PhotonNetwork.JoinLobby())
-				RefreshRoomList();
-			break;
-		case "JoinRandomRoom":
-			if(PhotonNetwork.JoinLobby())
-				JoinRandomRoomButton ();
-			break;
-		case "Exit":
-			Application.Quit ();
-			break;
-		}
-	}
+        bool available_room = false;
 
-	void RefreshRoomList()
-	{
-		if (roomPrefabs.Count > 0) {
-			for (int i = 0; i < roomPrefabs.Count; i++) {
-				Destroy (roomPrefabs[i]);
-			}
+        foreach (RoomInfo RI in PhotonNetwork.GetRoomList())
+        {
+            if (roomName == RI.name)
+            {
+                available_room = true;
+                break;
+            }
+            else
+            {
+                available_room = false;
+            }
+        }
 
-			roomPrefabs.Clear ();
-		}
-
-		for (int i = 0; i < PhotonNetwork.GetRoomList ().Length; i++) {
-			GameObject g = Instantiate(roomPrefab);
-			g.transform.SetParent (roomPrefab.transform.parent);
-
-			g.GetComponent<RectTransform> ().localScale = roomPrefab.GetComponent<RectTransform> ().localScale;
-			g.GetComponent<RectTransform> ().localPosition = new Vector3(roomPrefab.GetComponent<RectTransform>().localPosition.x,roomPrefab.GetComponent<RectTransform>().localPosition.y - (i*50),roomPrefab.GetComponent<RectTransform>().localPosition.z);
-			g.transform.FindChild("Room_Name_Text").GetComponent<Text>().text = PhotonNetwork.GetRoomList()[i].name;
-			g.transform.FindChild ("Room_Info").GetComponent<Text> ().text = PhotonNetwork.GetRoomList () [i].playerCount + "/" + PhotonNetwork.GetRoomList () [i].maxPlayers;
-
-			g.transform.FindChild ("Join").GetComponent<Button> ().onClick.AddListener (() => { JoinRoom(g.transform.FindChild("Room_Name_Text").GetComponent<Text>().text);});
-
-			g.SetActive (true);
-			roomPrefabs.Add (g);
-		}
-	}
-
-	void JoinRandomRoomButton()
-	{
-		if (PhotonNetwork.GetRoomList ().Length > 0) {
-			PhotonNetwork.JoinRandomRoom ();
-		} else {
-			Debug.LogError ("No Rooms");
-		}
-	}
-
-
-	void JoinRoom(string roomName){
-
-		bool available_room = false;
-
-		foreach (RoomInfo RI in PhotonNetwork.GetRoomList()) {
-			if (roomName == RI.name) {
-				available_room = true;
-				break;			
-			} else {
-				available_room = false;
-			}
-		}
-
-		if (available_room)
-			PhotonNetwork.JoinRoom (roomName);
-		else
-			Debug.Log ("No such room.");
-	}
-
-	void OnGUI()
-	{
-		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
-	}
-
-	public override void OnJoinedLobby()
-	{
-		Debug.Log ("fail");
-		Invoke("RefreshRoomList", 0.1f);
-	}
-
-	void OnPhotonJoinRoomFailed()
-	{
-		Debug.Log ("fail: ");
-	}
-
-	void OnJoinedRoom()
-	{
-
-		Debug.Log ("Room joined");
-        PhotonNetwork.LoadLevel(1);
-        StartSpawnProcess(0f);
+        if (available_room)
+            PhotonNetwork.JoinRoom(roomName);
+        else
+            Debug.Log("No such room.");
     }
 
-	void OnCreatedRoom()
-	{
-		Debug.Log ("Room created.");
-	}
-
-    void StartSpawnProcess (float respawnTime)
+    void OnGUI()
     {
-        sceneCamera.enabled = true;
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    void OnPhotonJoinRoomFailed()
+    {
+        Debug.Log("fail: ");
+    }
+
+    void OnJoinedRoom()
+    {
+
+        Debug.Log("Room joined");
+        if (PhotonNetwork.room.playerCount == 1)
+        {
+            Debug.Log("We load the 'Room for 1' ");
+
+            // #Critical
+            // Load the Room Level. 
+            PhotonNetwork.LoadLevel("Level1");
+        }
+        //SpawnSpot[] spawnSpots = GameObject.FindObjectsOfType<SpawnSpot>();
+        //Debug.Log(PhotonNetwork.playerName);
+
+        //Debug.Log(spawnSpots.ToString());
+
+        //if (spawnSpots == null)
+        //{
+        //    Debug.Log("Nope.");
+        //}
+
+        //int index = Random.Range(0, spawnPoints.Length);
+        //SpawnSpot mySpawnSpot = spawnSpots[Random.Range(1, spawnSpots.Length)];
+        //StartSpawnProcess(0f);
+    }
+
+    void OnCreatedRoom()
+    {
+        Debug.Log("Room created.");
+    }
+
+    void StartSpawnProcess(float respawnTime)
+    {
         StartCoroutine("SpawnPlayer", respawnTime);
     }
 
@@ -163,10 +174,64 @@ public class PhotonNetworkManager1 : Photon.PunBehaviour {
     {
         yield return new WaitForSeconds(respawnTime);
 
-        int index = Random.Range(0, spawnPoints.Length);
+
         player = PhotonNetwork.Instantiate("Dungeoneer", Vector3.zero, Quaternion.identity, 0);
-        sceneCamera.enabled = false;
         player.transform.FindChild("FirstPersonCharacter").gameObject.SetActive(true);
         player.transform.GetComponent<CharacterController>().enabled = true;
     }
+
+    #endregion
+
+    #region Public Methods
+
+    public void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings(_gameVersion);
+    }
+
+    public void ButtonEvents(string EVENT)
+    {
+        switch (EVENT)
+        {
+            case "CreateRoom":
+                if (PhotonNetwork.JoinLobby())
+                {
+
+                    RoomOptions RO = new RoomOptions();
+                    RO.MaxPlayers = 4;
+                    PhotonNetwork.CreateRoom(room_name.text, RO, TypedLobby.Default);
+
+                }
+                break;
+            case "RefreshButton":
+                if (PhotonNetwork.JoinLobby())
+                    RefreshRoomList();
+                break;
+            case "JoinRandomRoom":
+                if (PhotonNetwork.JoinLobby())
+                    JoinRandomRoomButton();
+                break;
+            case "Exit":
+                Application.Quit();
+                break;
+        }
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("fail");
+        Invoke("RefreshRoomList", 0.1f);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Launcher: OnConnectedToMaster() was called by PUN");
+    }
+
+    public override void OnDisconnectedFromPhoton()
+    {
+        Debug.LogWarning("Launcher: OnDisconnectedFromPhoton() was called by PUN.");
+    }
+    #endregion
 }
+
